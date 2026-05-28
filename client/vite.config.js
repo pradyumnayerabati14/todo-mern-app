05e46c599d83d7c6f012bc05d0346314d94faa7c
pradyumnayerabati14 <107753860+pradyumnayerabati14@users.noreply.github.com>
2026-05-28 18:53:08 -0400

    Added docker files

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5001'
+      '/api': process.env.VITE_API_PROXY_TARGET || 'http://localhost:5001'
     }
   }
-})
+});
